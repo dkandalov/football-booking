7d9b5b78c71e0eaffb5d53f21c8e99ab121f3a2b
Dmitry Kandalov <dmitry.kandalov@springernature.com>
2017-09-13 12:54:07 +0100

    get the booking page

--- src/footballbooking/Main.kt
+++ src/footballbooking/Main.kt
@@ -1,11 +1,17 @@
 package footballbooking
 
-import com.thoughtworks.xstream.XStream
-import com.thoughtworks.xstream.io.xml.XppDriver
 import org.http4k.client.ApacheClient
-import org.http4k.core.Method.GET
-import org.http4k.core.Request
+import org.http4k.core.*
+import org.http4k.core.Method.*
+import org.http4k.core.cookie.Cookie
 import org.http4k.core.cookie.cookies
+import org.http4k.filter.ClientFilters
+import org.http4k.filter.DebuggingFilters
+import org.http4k.filter.cookie.BasicCookieStorage
+import org.http4k.filter.cookie.CookieStorage
+import org.http4k.filter.cookie.LocalCookie
+import java.time.Clock
+import java.time.LocalDateTime
 
 fun main(args: Array<String>) {
     // home page: https://hsp.kingscross.co.uk
@@ -15,15 +21,50 @@ fun main(args: Array<String>) {
     // username: anuratransfersdev2@gmail.com
     // password: ...
 
-    val xStream = XStream(XppDriver())
-    val httpClient = ApacheClient()
+    val httpClient: HttpHandler =
+        ClientFilters.SetHostFrom(Uri.of("https://hsp.kingscross.co.uk"))
+            .then(Cookies())
+            .then(DebuggingFilters.PrintRequestAndResponse())
+            .then(ApacheClient())
 
-    val homePageRequest = Request(GET, "https://hsp.kingscross.co.uk").printed()
+    val homePageRequest = Request(GET, "/")
+    httpClient.invoke(homePageRequest)
 
-    httpClient(homePageRequest).let { response ->
-        response.headers.forEach { it.printed() }
-        response.cookies()
-    }
+    val loginPageRequest = Request(GET, "/Accounts/Login.aspx")
+    httpClient.invoke(loginPageRequest)
+
+    val signInRequest = Request(POST, "/Services/Commercial/api/security/validatelogin.json")
+        .body("{Email: \"anuratransfersdev2@gmail.com\", Password: \"...\", PersistCookie: false}")
+    httpClient.invoke(signInRequest)
+
+    val addBookingPageRequest = Request(GET, "/tools/commercial/muga/addsinglebooking.aspx")
+    httpClient.invoke(addBookingPageRequest)
 }
+
+object Cookies {
+    operator fun invoke(clock: Clock = Clock.systemDefaultZone(),
+                        storage: CookieStorage = BasicCookieStorage()): Filter = Filter { next ->
+        { request ->
+            val now = clock.now()
+            removeExpired(now, storage)
+            val response = next(request.withLocalCookies(storage))
+            storage.store(response.cookies().map { LocalCookie(it, now) })
+            response
+        }
+    }
+
+    private fun Request.withLocalCookies(storage: CookieStorage) = storage.retrieve()
+        .map { it.cookie }
+        .fold(this, { r, cookie -> r.cookie(cookie.name, cookie.value) })
+
+    private fun removeExpired(now: LocalDateTime, storage: CookieStorage)
+        = storage.retrieve().filter { it.isExpired(now) }.forEach { storage.remove(it.cookie.name) }
+
+    private fun Clock.now() = LocalDateTime.ofInstant(instant(), zone)
+}
+
+fun Request.cookie(name: String, value: String): Request = replaceHeader("Cookie", cookies().plus(Cookie(name, value)).toCookieString())
+
+private fun List<Cookie>.toCookieString() = map { "${it.name}=${it.value}" }.joinToString("; ")
 
 fun <T> T.printed(): T = this.apply { println(this) }
