788bd37d3de81d7e26165e691d70e93d22e2f6ab
Dmitry Kandalov <dmitry.kandalov@springernature.com>
2017-09-13 13:22:10 +0100

    find available slots and book

--- src/footballbooking/Main.kt
+++ src/footballbooking/Main.kt
@@ -3,6 +3,7 @@ package footballbooking
 import org.http4k.client.ApacheClient
 import org.http4k.core.*
 import org.http4k.core.Method.*
+import org.http4k.format.Gson.auto
 import org.http4k.core.cookie.Cookie
 import org.http4k.core.cookie.cookies
 import org.http4k.filter.ClientFilters
@@ -17,6 +18,8 @@ fun main(args: Array<String>) {
     // home page: https://hsp.kingscross.co.uk
     // login page: https://hsp.kingscross.co.uk/Accounts/Login.aspx
     // booking page: https://hsp.kingscross.co.uk/tools/commercial/muga/addsinglebooking.aspx
+    // post to list sessions: https://hsp.kingscross.co.uk/Services/Commercial/api/muga/ListAvailableSessions.json
+    // post to add booking: https://hsp.kingscross.co.uk/Services/Commercial/api/muga/AddBooking.json
 
     // username: anuratransfersdev2@gmail.com
     // password: ...
@@ -39,7 +42,39 @@ fun main(args: Array<String>) {
 
     val addBookingPageRequest = Request(GET, "/tools/commercial/muga/addsinglebooking.aspx")
     httpClient.invoke(addBookingPageRequest)
+
+    val footballGUID = "50ba1b7a-67f4-4c8d-a575-7dc8b5a43a30"
+    val listSessionsRequest = Request(POST, "/Services/Commercial/api/muga/ListAvailableSessions.json")
+        .body("{BookingDate: \"2017-09-18T00:00:00.000Z\", ActivityTypeGuid: \"$footballGUID\"}")
+    val response = httpClient.invoke(listSessionsRequest)
+
+    val bookingSessions = Body.auto<BookingSessions>().toLens().extract(response)
+    val session = bookingSessions.Data.find { it.Availability == 0 }.printed()
+    session ?: error("no session")
+
+    val bookSessionRequest = Request(POST, "https://hsp.kingscross.co.uk/Services/Commercial/api/muga/AddBooking.json")
+        .body("{" +
+            "\"ActivityTypeGuid\":\"$footballGUID\"," +
+            "\"SessionGuid\":\"${session.Guid}\"," +
+            "\"Date\":\"2017-09-18T00:00:00.000Z\"" +
+            "}")
+    httpClient.invoke(bookSessionRequest)
+
+    // relied with
+    // {"Code":200,"Data":{"Guid":"65295e82-1373-43eb-bda3-31e0ac8e6635"}}
 }
+
+// result generated from /json
+
+data class BookingSessions(val Code: Int, val Data: List<Slot>)
+
+data class Slot(
+    val Guid: String,
+    val Name: String,
+    val StartDateTime: String,
+    val EndDateTime: String,
+    val Availability: Int
+)
 
 object Cookies {
     operator fun invoke(clock: Clock = Clock.systemDefaultZone(),
