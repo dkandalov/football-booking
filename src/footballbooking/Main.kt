27b47b09ca6724378d90ed45a9678a319e77988a
David Denton <denton.david@gmail.com>
2017-09-27 13:08:45 +0100

    Added Data class representations of String concatenated values

--- src/footballbooking/Main.kt
+++ src/footballbooking/Main.kt
@@ -11,6 +11,7 @@ import org.http4k.core.Uri
 import org.http4k.core.cookie.Cookie
 import org.http4k.core.cookie.cookies
 import org.http4k.core.then
+import org.http4k.core.with
 import org.http4k.filter.ClientFilters
 import org.http4k.filter.DebuggingFilters
 import org.http4k.filter.TrafficFilters
@@ -21,6 +22,27 @@ import org.http4k.format.Gson.auto
 import org.http4k.traffic.ReadWriteCache
 import java.time.Clock
 import java.time.LocalDateTime
+
+data class Login(val Email: String, val Password: String, val PersistCookie: Boolean = true)
+
+data class ListSessions(val BookingDate: String, val ActivityTypeGuid: String)
+
+data class BookSlot(val ActivityTypeGuid: String, val SessionGuid: String, val Date: String)
+
+data class BookingSessions(val Code: Int, val Data: List<Slot>) {
+    fun findSlot() = Data.find { it.Availability == 0 }
+}
+
+data class Slot(
+    val Guid: String,
+    val Name: String,
+    val StartDateTime: String,
+    val EndDateTime: String,
+    val Availability: Int
+) {
+    fun book(guid: String) = BookSlot(guid, Guid, StartDateTime)
+}
+
 
 fun main(args: Array<String>) {
     // home page: https://hsp.kingscross.co.uk
@@ -47,8 +69,13 @@ fun main(args: Array<String>) {
     val loginPageRequest = Request(GET, "/Accounts/Login.aspx")
     httpClient(loginPageRequest)
 
+    val loginLens = Body.auto<Login>().toLens()
+    val listSessionsLens = Body.auto<ListSessions>().toLens()
+    val bookingSessionsLens = Body.auto<BookingSessions>().toLens()
+    val bookSlotLens = Body.auto<BookSlot>().toLens()
+
     val signInRequest = Request(POST, "/Services/Commercial/api/security/validatelogin.json")
-        .body("{Email: \"anuratransfersdev2@gmail.com\", Password: \"\", PersistCookie: false}")
+        .with(loginLens of Login("anuratransfersdev2@gmail.com", "Anura123"))
     httpClient(signInRequest)
 
     val addBookingPageRequest = Request(GET, "/tools/commercial/muga/addsinglebooking.aspx")
@@ -56,36 +83,20 @@ fun main(args: Array<String>) {
 
     val footballGUID = "50ba1b7a-67f4-4c8d-a575-7dc8b5a43a30"
     val listSessionsRequest = Request(POST, "/Services/Commercial/api/muga/ListAvailableSessions.json")
-        .body("{BookingDate: \"2017-10-03T00:00:00.000Z\", ActivityTypeGuid: \"$footballGUID\"}")
+        .with(listSessionsLens of ListSessions("2017-10-03T00:00:00.000Z", footballGUID))
     val response = httpClient(listSessionsRequest)
 
-    val bookingSessions = Body.auto<BookingSessions>().toLens().extract(response)
-    val session = bookingSessions.Data.find { it.Availability == 0 }.printed()
+    val bookingSessions = bookingSessionsLens.extract(response)
+    val session = bookingSessions.findSlot().printed()
     session ?: error("no session")
 
-//    val bookSessionRequest = Request(POST, "https://hsp.kingscross.co.uk/Services/Commercial/api/muga/AddBooking.json")
-//        .body("{" +
-//            "\"ActivityTypeGuid\":\"$footballGUID\"," +
-//            "\"SessionGuid\":\"${session.Guid}\"," +
-//            "\"Date\":\"2017-09-18T00:00:00.000Z\"" +
-//            "}")
+    val bookSessionRequest = Request(POST, "/Services/Commercial/api/muga/AddBooking.json")
+        .with(bookSlotLens of session.book(footballGUID))
 //    httpClient(bookSessionRequest)
 
     // relied with
     // {"Code":200,"Data":{"Guid":"65295e82-1373-43eb-bda3-31e0ac8e6635"}}
 }
-
-// result generated from /json
-
-data class BookingSessions(val Code: Int, val Data: List<Slot>)
-
-data class Slot(
-    val Guid: String,
-    val Name: String,
-    val StartDateTime: String,
-    val EndDateTime: String,
-    val Availability: Int
-)
 
 object Cookies {
     operator fun invoke(clock: Clock = Clock.systemDefaultZone(),
@@ -113,4 +124,4 @@ fun Request.cookie(name: String, value: String): Request = replaceHeader("Cookie
 
 private fun List<Cookie>.toCookieString() = map { "${it.name}=${it.value}" }.joinToString("; ")
 
-fun <T : Any?> T.printed() = this?.run(::println)
+fun <T : Any?> T.printed(): T? = this?.apply(::println)
